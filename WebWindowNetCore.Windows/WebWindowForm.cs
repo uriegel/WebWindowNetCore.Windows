eed8f546cff1f5c021b118be6fa73f6fb785b013
Uwe Riegel <uriegel@hotmail.de>
2023-10-29 14:51:54 +0100

    Resizing without titlebar

--- WebWindowNetCore.Windows/WebWindowForm.cs
+++ WebWindowNetCore.Windows/WebWindowForm.cs
@@ -23,6 +23,17 @@ public class WebWindowForm : Form
     {
         webView = new WebView2();
         ((System.ComponentModel.ISupportInitialize)(webView)).BeginInit();
+
+        this.ControlBox = false;
+this.Text = String.Empty;
+        
+        
+        FormBorderStyle = FormBorderStyle.Sizable;
+
+
+
+
+
         SuspendLayout();
         // 
         // webView
@@ -51,7 +62,10 @@ public class WebWindowForm : Form
         Controls.Add(webView);
         Name = "WebWindow";
 
-        Text = settings?.Title;
+        //Text = settings?.Title;
+
+
+
         ((System.ComponentModel.ISupportInitialize)(webView)).EndInit();
         ResumeLayout(false);
 
@@ -144,6 +158,97 @@ public class WebWindowForm : Form
         }
     }
 
+    const uint WM_NCCALCSIZE = 0x83;
+
+    protected override void WndProc(ref Message m)
+    {
+        //Don't style window in designer...
+        if (DesignMode)
+            base.WndProc(ref m);
+
+        //Handle Message
+        switch ((uint)m.Msg)
+        {
+            case WM_NCCALCSIZE:
+                WmNCCalcSize(ref m); 
+                break;
+
+            default: 
+                base.WndProc(ref m); 
+                break;
+        }
+    }
+
+    [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    struct RECT
+    {
+        public int left, top, right, bottom;
+    }
+
+     [System.Runtime.InteropServices.DllImport("user32.dll", ExactSpelling = true)]
+    [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        static extern bool GetWindowRect(
+        IntPtr hwnd,
+        out  RECT lpRect
+        );
+
+    [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    struct NCCALCSIZE_PARAMS
+    {
+        public RECT rgrc0, rgrc1, rgrc2;
+        public WINDOWPOS lppos;
+    }        
+
+ [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    struct WINDOWPOS
+    {
+        public IntPtr hwnd;
+        public IntPtr hwndinsertafter;
+        public int x, y, cx, cy;
+        public int flags;
+    }
+//WM_NCCALCSIZE
+    private void WmNCCalcSize(ref Message m)
+    {
+        //Get Window Rect
+        RECT formRect = new RECT();
+        GetWindowRect(m.HWnd, out formRect);
+
+        //Check WPARAM
+        if (m.WParam != IntPtr.Zero)    //TRUE
+        {
+            //When TRUE, LPARAM Points to a NCCALCSIZE_PARAMS structure
+            var nccsp = (NCCALCSIZE_PARAMS)System.Runtime.InteropServices.Marshal.PtrToStructure(m.LParam, typeof(NCCALCSIZE_PARAMS));
+
+            //We're adjusting the size of the client area here. Right now, the client area is the whole form.
+            //Adding to the Top, Bottom, Left, and Right will size the client area.
+            nccsp.rgrc0.top += 3;      //30-pixel top border
+            nccsp.rgrc0.bottom -= 5;    //4-pixel bottom (resize) border
+            nccsp.rgrc0.left += 5;      //4-pixel left (resize) border
+            nccsp.rgrc0.right -= 5;     //4-pixel right (resize) border
+
+            //Set the structure back into memory
+            System.Runtime.InteropServices.Marshal.StructureToPtr(nccsp, m.LParam, true);
+        }
+        else    //FALSE
+        {
+            //When FALSE, LPARAM Points to a RECT structure
+            var clnRect = (RECT)System.Runtime.InteropServices.Marshal.PtrToStructure(m.LParam, typeof(RECT));
+
+            //Like before, we're adjusting the rectangle...
+            //Adding to the Top, Bottom, Left, and Right will size the client area.
+            clnRect.top -= 4;      //30-pixel top border
+            clnRect.bottom -= 4;    //4-pixel bottom (resize) border
+            clnRect.left += 4;      //4-pixel left (resize) border
+            clnRect.right -= 4;     //4-pixel right (resize) border
+
+            //Set the structure back into memory
+            System.Runtime.InteropServices.Marshal.StructureToPtr(clnRect, m.LParam, true);
+        }
+
+        //Return Zero
+        m.Result = IntPtr.Zero;
+    }
 
     WebView2 webView;
     bool initialized;
