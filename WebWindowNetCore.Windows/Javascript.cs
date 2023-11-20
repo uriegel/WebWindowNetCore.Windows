c9e4f9542c830297eb7fa968d3cfc0da4a686620
Uwe Riegel <uriegel@hotmail.de>
2023-11-20 21:17:36 +0100

    new Version 4

--- WebWindowNetCore.Windows/Javascript.cs
+++ WebWindowNetCore.Windows/Javascript.cs
@@ -1,13 +1,14 @@
 using System.Runtime.InteropServices;
 using System.Text.Json;
-using LinqTools;
 using static AspNetExtensions.Core;
 
 namespace WebWindowNetCore;
 
 [ComVisible(true)]
-public class Callback(WebWindowForm parent)
+public class Callback
 {
+    public Callback(WebWindowForm parent) => this.parent = parent;
+
     public void ShowDevtools() => parent.ShowDevtools();
     public void MaximizeWindow() => parent.MaximizeWindow();
     public void MinimizeWindow() => parent.MinimizeWindow();
@@ -17,6 +18,8 @@ public class Callback(WebWindowForm parent)
         => JsonSerializer.Deserialize<FileListType>(fileList, JsonWebDefaults)
             .Map(flt =>
                 parent.DragStart(flt!.Path, flt!.FileList));
+
+    readonly WebWindowForm parent;
 }
 
 record FileListType(string[] FileList, string Path);
